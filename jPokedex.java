8f4309319bbfb69fd5ab4b19b6417a1f604cf21a
Gatene <Gatene1@iCloud.com>
2022-02-10 15:26:55 -0500

    Testing for coaching purposes

--- jPokedex.java
+++ jPokedex.java
@@ -2,22 +2,22 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.Properties;
 
-class Pokedex
+public class jPokedex
 {
 	public static String cResetBg = "\u001B[40m";
 	public static String cBlueBg = "\u001B[44m";
 	public static String cReset = "\u001B[0m";
 	public static String cWhite = "\u001B[37m";
-	public static String accE = "\u00E9";
 	public static String clearScr = "\033[H\033[2J";
 
+	
 	public static void main(String[] args) throws Exception
 	{
 		Scanner waitingInput = new Scanner(System.in);
 
 		System.out.println(clearScr);
 		System.out.flush();
-		System.out.println(cBlueBg + cWhite + "Sort by Pokemon's Number" + cReset + cResetBg);
+		System.out.println(cBlueBg + "Sort by Pokemon's Number" + cResetBg);
 		System.out.println("Sort by Pokemon's Name");
 		System.out.println("Sort by Pokemon's Type1");
 		System.out.println("Sort by Pokemon's Type2");
@@ -27,6 +27,9 @@ class Pokedex
 		System.out.println("Sort by Weight\n");
 
 		System.out.println("Use arrow keys to make selection and press Enter to confirm");
-		String menuInput = waitingInput.nextLine();
+		String menuInput = waitingInput.next();
+		System.out.println(waitingInput.hasNext());
+		
 	}
 }
+
